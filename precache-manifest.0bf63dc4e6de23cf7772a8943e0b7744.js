d58b7811330e36807c4127cb60258d9657f58d52
Kush <kushgupta2020@gmail.com>
2020-01-23 07:46:35 +0530

    Updates

---
+++ precache-manifest.0bf63dc4e6de23cf7772a8943e0b7744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32420a4482a33d1a71495ce5eb5f3735",
+    "url": "/lightsout/index.html"
+  },
+  {
+    "revision": "dd88b82ad59a8ff738b8",
+    "url": "/lightsout/static/css/main.5bf1b0db.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/lightsout/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "dd88b82ad59a8ff738b8",
+    "url": "/lightsout/static/js/main.47aab8a6.chunk.js"
+  },
+  {
+    "revision": "4ee06c896b3f619c198a",
+    "url": "/lightsout/static/js/runtime~main.f085888c.js"
+  }
+]);
